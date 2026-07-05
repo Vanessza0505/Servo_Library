4135784ccf665ef02184b82029e5d55bf9c013ab
Vanessza <schabvanessza@gmail.com>
2026-07-05 20:43:18 +0200

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,14 +1,9 @@
 #![no_std]
 
 use embedded_hal::pwm::SetDutyCycle;
-use esp_hal::delay::Delay;
+use embassy_time::{Duration, Timer};
 use esp_hal::ledc::channel::Channel;
 use esp_hal::ledc::HighSpeed;
-
-pub enum ServoError {
-    InvalidAngle,
-    CommunicationError,
-}
 
 
 pub struct Servo {
@@ -17,6 +12,7 @@ pub struct Servo {
     max_duty: u32,
     max_angle: u32,
     angle: u32,
+    open_time: u64
 }
 
 impl Servo {
@@ -26,6 +22,7 @@ impl Servo {
         max_duty: u32,
         max_angle: u32,
         angle: u32,
+        open_time: u64
     ) -> Self {
         Self {
             channel,
@@ -33,33 +30,39 @@ impl Servo {
             max_duty,
             max_angle,
             angle,
+            open_time
         }
     }
 
-
-    pub fn rotate(&mut self, angle: u32) -> Result<(), ServoError> {
-        if angle > self.max_angle {
-            return Err(ServoError::InvalidAngle);
-        }
-
+    //this function communicate the hardwear and actually rotate the servo
+    pub fn rotate(&mut self, angle: u32){
         let duty_gap = self.max_duty - self.min_duty;
         let duty = self.min_duty + (angle * duty_gap) / self.max_angle;
-
-        self.channel
-            .set_duty_cycle(duty as u16)
-            .map_err(|_| ServoError::CommunicationError)
+        self.channel.set_duty_cycle(duty as u16);
     }
 
-    
-    pub fn feed(&mut self, delay: &Delay) -> Result<(), ServoError> {
+    //this is open the boat door
+    pub async fn open(&mut self){
         for deg in 0..=self.angle {
             self.rotate(deg);
-            delay.delay_millis(15);
+            Timer::after(Duration::from_millis(15)).await;
         }
+    }
+
+
+    // this is close the boat door
+    pub async fn close(&mut self){
         for deg in (0..=self.angle).rev() {
             self.rotate(deg);
-            delay.delay_millis(15);
+            Timer::after(Duration::from_millis(15)).await;
         }
-        Ok(())
+    }
+
+
+    // this is the feeding function when the fisherman click the button to feed the fish this made the door to open with the servo helping and it will close
+    pub async fn feed(&mut self){
+        self.open().await;
+        Timer::after(Duration::from_secs(self.open_time)).await; //opening time 
+        self.close().await;
     }
 }
